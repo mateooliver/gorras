bfc996b6fe5ee223aa1be9b7d88b65631a7830ab
Mateo Oliver <mateooliver@MacBook-Air-de-Mateo.local>
2022-03-09 13:04:15 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
