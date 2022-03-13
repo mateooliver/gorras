326324507f3d65f15ee00edb261def2fd6b8e922
Mateo Oliver <mateooliver@MacBook-Air-de-Mateo.local>
2022-03-13 13:43:18 -0300

    Segunda entrega navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -6,23 +5,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <header>
         <Navbar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    </header>
   );
 }
 
